df7133255e8017ab9f7137a070cc02daec116889
DESKTOP-B8G5MKQ\swegu <kurtare95@gmail.com>
2020-10-10 00:40:55 +0200

    We have AutoScrolling object woop

---
+++ GameJam_2020/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScript : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("break");
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ GameJam_2020/Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        controller.Move(Vector3.forward * speed * Time.deltaTime);
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            controller.Move(direction * speed * Time.deltaTime);
+
+        }
+    }
+}
