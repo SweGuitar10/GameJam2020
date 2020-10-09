bb20d63ad7eb2b14a52ac36a026ba0888d2201e5
DESKTOP-B8G5MKQ\swegu <kurtare95@gmail.com>
2020-10-10 00:46:52 +0200

    Different speeds for movement

--- GameJam_2020/Assets/Scripts/Movement.cs
+++ GameJam_2020/Assets/Scripts/Movement.cs
@@ -9,7 +9,8 @@ public class Movement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float forwardSpeed = 25f;
+    public float horizontalSpeed = 10f;
     
 
     // Update is called once per frame
@@ -19,12 +20,12 @@ public class Movement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        controller.Move(Vector3.forward * speed * Time.deltaTime);
+        controller.Move(Vector3.forward * forwardSpeed * Time.deltaTime);
 
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            controller.Move(direction * speed * Time.deltaTime);
+            controller.Move(direction * horizontalSpeed * Time.deltaTime);
 
         }
     }
