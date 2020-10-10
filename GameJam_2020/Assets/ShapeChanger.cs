4196393177bd1cdcf52b347eb20e5106aa831bbf
DESKTOP-B8G5MKQ\swegu <kurtare95@gmail.com>
2020-10-10 11:02:25 +0200

    Shapeshifting is go

---
+++ GameJam_2020/Assets/ShapeChanger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeChanger : MonoBehaviour
+{
+    public GameObject cube, sphere, triangle;
+    int shape;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            shape = 0;
+            changeShape(shape);
+           
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            shape = 1;
+            changeShape(shape);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            shape = 2;
+            changeShape(shape);
+        }
+    }
+
+    private void changeShape(int shape)
+    {
+        switch(shape)
+        {
+            case 0:
+                cube.SetActive(true);
+                sphere.SetActive(false);
+                triangle.SetActive(false);
+                break;
+            case 1:
+                cube.SetActive(false);
+                sphere.SetActive(true);
+                triangle.SetActive(false);
+                break;
+            case 2:
+                cube.SetActive(false);
+                sphere.SetActive(false);
+                triangle.SetActive(true);
+                break;
+            default:
+                Debug.Log("Shapeshift error!");
+                break;
+        }
+    }
+}
